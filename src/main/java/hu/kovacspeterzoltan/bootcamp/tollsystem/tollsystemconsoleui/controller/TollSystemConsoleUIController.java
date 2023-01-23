bec12ff1d7185fb4e5fc84702eacf24b1246fcf4
webkovacs <webkovacs@gmail.com>
2023-01-24 00:04:55 +0100

    dátum javítása a storage-ben + megjelenítés

--- src/main/java/hu/kovacspeterzoltan/bootcamp/tollsystem/tollsystemconsoleui/controller/TollSystemConsoleUIController.java
+++ src/main/java/hu/kovacspeterzoltan/bootcamp/tollsystem/tollsystemconsoleui/controller/TollSystemConsoleUIController.java
@@ -2,10 +2,15 @@ package hu.kovacspeterzoltan.bootcamp.tollsystem.tollsystemconsoleui.controller;
 
 import hu.kovacspeterzoltan.bootcamp.tollsystem.tollsystemconsoleui.view.TollSystemConsoleUIView;
 import hu.kovacspeterzoltan.bootcamp.tollsystemapp.api.MotorwayVignetteAPI;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Scanner;
 
 public class TollSystemConsoleUIController {
     private MotorwayVignetteAPI motorwayVignetteInteractor;
     private TollSystemConsoleUIView view;
+    private Scanner scanner;
 
     public TollSystemConsoleUIController() {
         view = new TollSystemConsoleUIView();
@@ -15,6 +20,31 @@ public class TollSystemConsoleUIController {
         this.motorwayVignetteInteractor = motorwayVignetteImp;
     }
     public void start() {
-        view.displayStartMessage();
+        scanner = new Scanner(System.in);
+        String action = null;
+        do {
+            action = getActionString();
+
+            if ("l".equals(action)) {
+                findAction();
+            }
+        } while (!"k".equals(action));
+    }
+    private String getActionString() {
+        String action;
+        view.displaySelectActionMessage();
+        action = scanner.next();
+        return action;
+    }
+
+    private void findAction() {
+        try {
+            view.displayRegistrationNumberInputMessage();
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("registrationNumber", scanner.next().toUpperCase());
+            motorwayVignetteInteractor.findByRegistrationNumber(jsonObject.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/hu/kovacspeterzoltan/bootcamp/tollsystem/tollsystemconsoleui/view/TollSystemConsoleUIView.java
+++ src/main/java/hu/kovacspeterzoltan/bootcamp/tollsystem/tollsystemconsoleui/view/TollSystemConsoleUIView.java
@@ -3,8 +3,11 @@ package hu.kovacspeterzoltan.bootcamp.tollsystem.tollsystemconsoleui.view;
 import org.json.JSONObject;
 
 public class TollSystemConsoleUIView {
-    public void displayStartMessage() {
-        System.out.println("Adja meg a keresett rendszámot:");
+    public void displaySelectActionMessage() {
+        System.out.println("Válasszon funkciót: Lekérdezés (l); Kilépés (k)");
+    }
+    public void displayRegistrationNumberInputMessage() {
+        System.out.println("Adja meg a rendszámot:");
     }
 
     public void displayMotorwayVignette(JSONObject responseJsonObject) {
